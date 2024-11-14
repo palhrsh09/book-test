b5c307639dce438163dc0991a1aa49e98e48e110
harsh <theharshpal18@gmail.com>
2024-11-14 21:55:47 +0530

    chnagesin db file

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 export const ConnectDB = () => {
-    mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookstore', {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
+    serverSelectionTimeoutMS: 10000 // Adjust timeout if needed
+}).then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
 }
